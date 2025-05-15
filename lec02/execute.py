4556009f15cfbe52337e833f3a0fbed416462522
joelittyab-git <joeittab@gmail.com>
2025-05-15 22:05:22 +0530

    execution file

---
+++ lec02/execute.py
@@ -0,0 +1,23 @@
+# from itertools import product
+
+# cartesian = product([True, False], repeat=3)
+
+# for t in cartesian:
+#      print(dict(zip(['P','Q','R'], t)))
+     
+     
+
+from model import LogicalInferenceEngine
+from logic import And, Symbol, Implication, Not
+
+P = Symbol("It is raining")
+Q = Symbol("Jack goes out")
+R = Symbol("It is a Tuesday")
+
+e = LogicalInferenceEngine(And(
+     Implication(And(R, Not(P)), Q),
+     Not(Q),
+     R    #P should retrun true
+))
+s = e.evaluate_for(P)
+print(s)
